726d48f3b9e661d89a05f4927797eca4c51a00b2
Nirjhor Nath <nirjhor.nath@gmail.com>
2024-03-13 16:30:04 -0400

    dumb down home page

--- src/app/features/home-view.tsx
+++ src/app/features/home-view.tsx
@@ -1,28 +1,3 @@
-import { Link } from "@tanstack/react-router";
-import { trpc } from "~app/lib/trpc";
-import { useSignOut } from "./sign-out";
-
 export const HomeView = () => {
-  const [data] = trpc.queryMe.useSuspenseQuery();
-
-  const signOut = useSignOut();
-
-  return (
-    <div>
-      <div className="bg-red-200 text-blue-700">Home View</div>
-      <p>server: {JSON.stringify(data)}</p>
-      {data ? (
-        <div>
-          <button onClick={() => signOut.mutate()}>Sign Out</button>
-        </div>
-      ) : (
-        <div>
-          <Link to="/sign-in">Sign In</Link>
-        </div>
-      )}
-      <div>
-        <button onClick={console.log}>Click Me</button>
-      </div>
-    </div>
-  );
+  return <div>Hi there</div>;
 };
