9b40b4db4eb01d957992f806651cf9e0ac51db23
Nirjhor Nath <nirjhor.nath@gmail.com>
2024-03-13 16:16:43 -0400

    add healthcheck api route

--- src/server/hono.ts
+++ src/server/hono.ts
@@ -6,6 +6,8 @@ import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
 export const hono = new Hono().basePath("/api");
 
+hono.get("/healthcheck", (ctx) => ctx.json({ ok: true }));
+
 hono.route("/auth", authRouter);
 
 hono.use("/trpc/*", (ctx) =>
